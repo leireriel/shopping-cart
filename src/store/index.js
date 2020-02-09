166841bd3f3a5954c8d7f2439b97fa11a9d27d7e
leire <leirericop@gmail.com>
2020-02-09 19:50:53 +0100

    feat(redux): create redux modules structure in each component

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
-import reducers from 'reducers';
+import allReducers from 'reducers';
 
-const store = createStore(reducers);
+const allowReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(allReducers, allowReduxDevTools);
 
 export default store;
