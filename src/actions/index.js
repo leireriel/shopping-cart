180f365a85040dcfb5b671b9c56a40e783a0d17f
leire <leirericop@gmail.com>
2020-02-10 18:02:10 +0100

    feat(assets): add assets from challenge

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export const addOne = number => ({ 
-  type: 'ADD_ONE',
-  payload: number
-});
-
-export const removeOne = () => ({ 
-  type: 'REMOVE_ONE'
-});
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import allReducers from 'reducers';
-import App from 'routes/App';
-import 'styles/index.scss';
+import App from './App';
 
-const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-render(
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
