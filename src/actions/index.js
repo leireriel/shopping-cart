6cf47d20f5bca34ca018517df5d6e76b92590605
leire <leirericop@gmail.com>
2020-02-13 21:40:35 +0100

    feat(refactor): refactor action product

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,5 @@
-import { SHIRT, MUG, CAP } from 'constants/index';
-
-export const actionShirt = (actionToPerform, price) => ({ 
-  type: SHIRT,
+export const actionProduct = ({actionToPerform, price, type}) => ({ 
+  type,
   actionToPerform,
   price
 });
-
-export const actionMug = (actionToPerform, price) => ({ 
-  type: MUG,
-  actionToPerform,
-  price
-});
-
-export const actionCap = (actionToPerform, price) => ({ 
-  type: CAP,
-  actionToPerform,
-  price
-});
-
-// TODO una sola function
