28ca81119833a55ddcb118b8dce9553c7f0e2883
leire <leirericop@gmail.com>
2020-02-14 18:04:14 +0100

    feat(refactor): move css folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from 'reducers';
 import App from './App';
-import 'css/main.css';
 
 const store = createStore(
   rootReducer,
