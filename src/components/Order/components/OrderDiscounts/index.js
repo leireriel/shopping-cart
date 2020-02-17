6a943e5c8231e6758860dcf80af889e9b343078f
Leire <lrico@fintonic.com>
2020-02-17 16:38:36 +0100

    feat(order): get discount values from redux and render

--- src/components/Order/components/OrderDiscounts/index.js
+++ src/components/Order/components/OrderDiscounts/index.js
@@ -2,47 +2,62 @@ import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 const OrderDiscounts = memo(() => {
-  const products = useSelector(state => state.products);
-  const arrOfProducts = Object.entries(products);
+  // const products = useSelector(state => state.products);
+  // const arrOfProducts = Object.entries(products);
 
-  let allDiscounts = [];
+  // let allDiscounts = [];
 
-  for (const product of arrOfProducts) {
-    const nameAndDiscount = [];
+  // for (const product of arrOfProducts) {
+  //   const nameAndDiscount = [];
 
-    if (product[1].discount > 0) {
-      const name = product[0];
-      const discount = product[1].discount;
+  //   if (product[1].discount > 0) {
+  //     const name = product[0];
+  //     const discount = product[1].discount;
   
-      nameAndDiscount.push(name, discount);
-      allDiscounts.push(nameAndDiscount);
-    }
-  };
+  //     nameAndDiscount.push(name, discount);
+  //     allDiscounts.push(nameAndDiscount);
+  //   }
+  // };
 
-  console.log(allDiscounts);
+  // console.log(allDiscounts);
 
-  let renderAllDiscounts = [];
+  // let renderAllDiscounts = [];
 
-  for (const discount of allDiscounts) {
-    const name = discount[0];
-    const amount = discount[1];
-    const discountCopies = useSelector(state => state.products[name].discountCopies);
+  // for (const discount of allDiscounts) {
+  //   const name = discount[0];
+  //   const amount = discount[1];
+  //   const discountCopies = useSelector(state => state.products[name].discountCopies);
     
-    renderAllDiscounts.push(
-      <li key='vuelta'>
-        <span>{discountCopies}</span>
-        <span>-{amount}€</span>
-      </li>
-    );
-  };
+  //   renderAllDiscounts.push(
+  //     <li key='vuelta'>
+  //       <span>{discountCopies}</span>
+  //       <span>-{amount}€</span>
+  //     </li>
+  //   );
+  // };
   
-  
+  const discountCopiesShirt = useSelector(state => state.products.Shirt?.discountCopies);
+  const discountCopiesMug = useSelector(state => state.products.Mug?.discountCopies);
+  const discountShirt = useSelector(state => state.products.Shirt.discount);
+  const discountMug = useSelector(state => state.products.Mug.discount);
   
   return (
     <div className='summary-discounts wrapper-half border'>
       <h2>Discounts</h2>
       <ul>
-        {renderAllDiscounts}
+        {discountCopiesShirt && (
+          <li key='0'>
+            <span>{discountCopiesShirt}</span>
+            <span>-{discountShirt}€</span>
+          </li>
+        )}
+        {discountCopiesMug && (
+          <li key='0'>
+            <span>{discountCopiesMug}</span>
+            <span>-{discountMug}€</span>
+          </li>
+        )}
+        {/* {renderAllDiscounts} */}
         <li><span>Promo code</span><span>0€</span></li>
       </ul>
     </div>
