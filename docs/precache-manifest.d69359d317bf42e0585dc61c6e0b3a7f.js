767a509c1293aed4b06b0ce4fdd719c78c1cf78b
leire <leirericop@gmail.com>
2020-02-17 22:48:54 +0100

    feat(publish): generate public folder docs

---
+++ docs/precache-manifest.d69359d317bf42e0585dc61c6e0b3a7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4230563fd9142020c24c376e31cb589",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a827af0262d4ca316f96",
+    "url": "./static/css/main.bee03df3.chunk.css"
+  },
+  {
+    "revision": "a01a79a18289181d3277",
+    "url": "./static/js/2.a43cb693.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.a43cb693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a827af0262d4ca316f96",
+    "url": "./static/js/main.4d0f5597.chunk.js"
+  },
+  {
+    "revision": "401bf1bca59f117e5514",
+    "url": "./static/js/runtime-main.d31b2714.js"
+  },
+  {
+    "revision": "778660f256a9687b256d247b22ba0d2f",
+    "url": "./static/media/background.778660f2.png"
+  },
+  {
+    "revision": "3cf74c1d28703eb35377259c6e1dc293",
+    "url": "./static/media/close_icon.3cf74c1d.svg"
+  }
+]);
